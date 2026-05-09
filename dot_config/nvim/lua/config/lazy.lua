c74a5814095d1f0171d1178382ef60f232bf86b4
BoChao1Zhang <3210191548@qq.com>
2026-05-09 23:27:18 +0800

    Update dotfiles from MacBook-Pro

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -1,24 +1,4 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local sitepath = vim.fn.stdpath("data") .. "/site"
-local tool_prefix = vim.env.DOTFILES_TOOL_PREFIX or vim.fn.expand("$HOME/.local/share/dotfiles/toolchain")
-
-local function prepend_path(path)
-  if path ~= "" and vim.fn.isdirectory(path) == 1 then
-    local parts = vim.split(vim.env.PATH or "", ":", { plain = true })
-    if not vim.tbl_contains(parts, path) then
-      vim.env.PATH = path .. ":" .. (vim.env.PATH or "")
-    end
-  end
-end
-
-local function repair_runtime_env()
-  prepend_path(vim.fn.expand("$HOME/.local/bin"))
-  prepend_path(tool_prefix .. "/bin")
-  vim.opt.rtp:prepend(sitepath)
-end
-
-repair_runtime_env()
-
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -33,17 +13,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "LazyDone",
-  callback = function()
-    repair_runtime_env()
-    local ok, treesitter_config = pcall(require, "nvim-treesitter.config")
-    if ok then
-      treesitter_config.setup({ install_dir = sitepath })
-    end
-  end,
-})
 
 require("lazy").setup({
   spec = {
@@ -62,22 +31,12 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  pkg = {
-    sources = { "lazy", "packspec" },
-  },
-  rocks = {
-    enabled = false,
-    hererocks = false,
-  },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
     rtp = {
-      paths = {
-        vim.fn.stdpath("data") .. "/site",
-      },
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
@@ -92,5 +51,3 @@ require("lazy").setup({
     },
   },
 })
-
-repair_runtime_env()
